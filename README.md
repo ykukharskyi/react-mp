3fba55aac2ad853a867549f485324649c1937aea
Yurii Kukharskyi <yurii.kukharskyi@thomsonreuters.com>
2019-03-11 18:43:36 +0200

    Task 1: Initialized React Project; Created simple React components in different ways

---
+++ README.md
@@ -0,0 +1,8 @@
+# React Mentoring Program
+
+#### Task 1: Setting up the environment
+
+```$bash
+$ npm init
+$ npm install express --save
+```
---
+++ static/app.js
@@ -0,0 +1,27 @@
+const helloByCreateElement = React.createElement('div', {}, 'Hello World');
+
+const HelloByFunction = () => {
+    return <div>Hello World</div>;
+};
+
+class HelloByComponent extends React.Component {
+    render() {
+        return <div>Hello World</div>;
+    }
+}
+
+class HelloByPureComponent extends React.PureComponent {
+    render() {
+        return <div>Hello World</div>;
+    }
+}
+
+ReactDOM.render(
+    <React.Fragment>
+        {helloByCreateElement}
+        <HelloByFunction/>
+        <HelloByComponent/>
+        <HelloByPureComponent/>
+    </React.Fragment>,
+    document.getElementById("container")
+);
